a4303e709fcc2a2b8967c12ef0efe2e0317f1ad4
ArchiXIII <archi.xiii@mail.ru>
2017-09-25 16:03:20 +0400

    Writed a class

--- src/main/java/com/archi/Numbers.java
+++ src/main/java/com/archi/Numbers.java
@@ -4,5 +4,20 @@ package com.archi;
  * Created by Черный on 25.09.2017.
  */
 public class Numbers {
+    public static void main(String[] args) {
+        odd();
+//        even();
+    }
 
+    public static void odd(){
+        for(int i = 241; i <= 1111; i+=2){
+            System.out.print(i + ", ");
+        }
+    }
+
+    public static void even(){
+        for(int i = 1112; i >= 242; i-=2){
+            System.out.print(i + ", ");
+        }
+    }
 }
